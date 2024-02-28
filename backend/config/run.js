50157b5dcc95691ede7dd7b7ff1ac881d6c1fef5
tnilaweera <161529446+tnilaweera@users.noreply.github.com>
2024-02-27 20:36:05 -0500

    Create run.js
    Bramely told me to do this

---
+++ backend/config/run.js
@@ -0,0 +1,13 @@
+module.exports = {
+            HOST: "217.21.78.182",
+            USER: "team1676_codesnopes",
+            PASSWORD: "oEr-8OPu)!)bMF)S",
+            DB: "team1676_apps",
+            dialect: "mysql",
+            pool: {
+              max: 5,
+              min: 0,
+              acquire: 30000,
+              idle: 10000
+            }
+};
